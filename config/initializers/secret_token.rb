c64686db2bc9843c9c8a1885407087edcc5676db
Jon Kern <jonkern@comcast.net>
2013-11-28 09:39:10 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GmapsMongo::Application.config.secret_key_base = '6410347846d3fc24d03967fbb70ae46e0bc0826c0c45c9ccdaac59e389b3c6b2a0ddccd48a85ff90838d590a0992bca6b81c64c469a09f8d70a0599e64cb0770'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GmapsMongo::Application.config.session_store :cookie_store, key: '_gmaps_mongo_session'
